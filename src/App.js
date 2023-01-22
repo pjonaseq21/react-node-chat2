2045006f48c722b6bf3fc5ac67abc8b462678d0b
pjonaseq21 <gracjan.chamulati2019@gmail.com>
2023-01-22 12:45:39 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Welcome(){
+ return <h1>Hello world </h1>;
 }
+function Body(){
+  return (
+    <>
 
-export default App;
+  <div>test</div>
+  <Button variant="success">Success</Button>
+
+  </>
+
+  )
+}
+const SiteComponents = {
+  Welcome,Body
+};
+export default SiteComponents;
